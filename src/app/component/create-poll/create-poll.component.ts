df77a68b370881b81a9dcdf696e8034f5e42b460
sabin <sabinshrestha292@gmail.com>
2018-02-27 15:59:40 +0300

    added validation to create new poll

--- src/app/component/create-poll/create-poll.component.ts
+++ src/app/component/create-poll/create-poll.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
-import {PollModel} from '../../model/pollModel';
-import {Router} from '@angular/router';
+import { PollModel } from '../../model/pollModel';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-create-poll',
@@ -10,10 +11,10 @@ import {Router} from '@angular/router';
 })
 export class CreatePollComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router: Router) { }
   addChoice: boolean = false;
   countOption = 2;
-  count=[1,2];
+  count = [1, 2];
   addChoiceError: String = "";
 
 
@@ -24,43 +25,55 @@ export class CreatePollComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({
       question: new FormControl(null, Validators.required),
+      choice0: new FormControl(null, Validators.required),
       choice1: new FormControl(null, Validators.required),
       choice2: new FormControl(null, Validators.required),
       choice3: new FormControl(null, Validators.required),
       choice4: new FormControl(null, Validators.required),
-      choice5: new FormControl(null, Validators.required)
+      startDate: new FormControl(null, Validators.required),
+      endDate: new FormControl(null, Validators.required),
+      isAnonymous: new FormControl(null, Validators.required)
     })
+  }
+
+  setDisable() {
+
+    console.log(this.form)
+    for (let i=0;i<this.count.length;i++) {
+      console.log("sedisable",this.form.get('choice'+i).value);
+      
+    }
   }
 
 
   addOption() {
-    console.log(this.addChoice)
+
     this.countOption++;
     this.count.push(this.countOption);
-    console.log("count", this.countOption)
     // if (this.countOption > 2) {
     //   this.addChoiceError = "You can only add upto 5 choices!";
     // }
 
   }
 
-  createPoll(){
-    let options=[];
+  createPoll() {
+
+    let options = [];
     options.push(this.form.value.choice1);
     options.push(this.form.value.choice2);
     options.push(this.form.value.choice3);
 
-    if(this.form.value.choice4!=null){
+    if (this.form.value.choice4 != null) {
       options.push(this.form.value.choice4);
     }
-    
-    let poll=new PollModel(this.form.value.question,options);
-    console.log(JSON.stringify(poll),"poll")
+
+    let poll = new PollModel(this.form.value.question, options);
+    console.log(JSON.stringify(poll), "poll")
 
   }
 
 
-  indexPoll(){
+  indexPoll() {
     this.router.navigateByUrl('/indexPoll');
   }
 
